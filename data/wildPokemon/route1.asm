d51ea88c1cedd11a6b26ada353562e064c9e0547
3DaysSereneGrace <37894575+3DaysSereneGrace@users.noreply.github.com>
2019-03-07 03:55:09 -0700

    Update route1.asm

--- data/wildPokemon/route1.asm
+++ data/wildPokemon/route1.asm
@@ -1,13 +1,13 @@
 Route1Mons:
 	db $19
-	db 3,PIDGEY
-	db 3,RATTATA
-	db 3,RATTATA
+	db 2,CATERPIE
+	db 2,WEEDLE
+	db 2,ZUBAT
 	db 2,RATTATA
 	db 2,PIDGEY
-	db 3,PIDGEY
-	db 3,PIDGEY
-	db 4,RATTATA
-	db 4,PIDGEY
-	db 5,PIDGEY
+	db 2,SPEAROW
+	db 2,NIDORANF
+	db 2,NIDORANM
+	db 2,JIGGLYPUFF
+	db 2,CLEFAIRY
 	db $00
